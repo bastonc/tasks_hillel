ac77ed3d40892a2223dfb968cf08970471fcfba7
Baston Sergey <bastonsv@gmail.com>
2022-10-02 00:12:19 +0300

    HW-23 Custom map refactor improved perfomans

--- CustomMap.py
+++ CustomMap.py
@@ -5,26 +5,16 @@ class CustomMap:
         self.func_value = func_value
 
     def custom_map(self):
-        stop_iter = 0
         output_dict = {}
-        key_val_tuple = iter(self.in_dict.items())
-        while not stop_iter:
-            try:
-                key_val_all_tuple = next(key_val_tuple)
-                output_dict.update({self.func_key(key_val_all_tuple[0]): self.func_value(key_val_all_tuple[1])})
-            except StopIteration:
-                stop_iter = 1
+        items = iter(self.in_dict.items())
+        for key_val_tuple in items:
+            output_dict.update({self.func_key(key_val_tuple[0]): self.func_value(key_val_tuple[1])})
         return output_dict
 
 
 def map_custom(in_dict: dict, func_key: object, func_value: object) -> dict:
-    stop_iter = 0
     output_dict = {}
-    key_val_tuple = iter(in_dict.items())
-    while not stop_iter:
-        try:
-            key_val_all_tuple = next(key_val_tuple)
-            output_dict.update({func_key(key_val_all_tuple[0]): func_value(key_val_all_tuple[1])})
-        except StopIteration:
-            stop_iter = 1
+    items = iter(in_dict.items())
+    for key_val_tuple in items:
+        output_dict.update({func_key(key_val_tuple[0]): func_value(key_val_tuple[1])})
     return output_dict
--- custom_map_example.py
+++ custom_map_example.py
@@ -1,11 +1,11 @@
 from CustomMap import CustomMap, map_custom
 
 
-def key_func(in_object):
+def key_func(in_object: object) -> object:
     return type(in_object)
 
 
-def value_func(in_object):
+def value_func(in_object: object) -> object:
     return type(in_object)
 
 
@@ -14,4 +14,4 @@ if __name__ == "__main__":
     print(CustomMap({True: 2, "two": 2}, key_func, value_func).custom_map())
 
     # Class in dataset and solution by function
-    print(map_custom({CustomMap({1: 2}, None, None): 2, "two": 2}, key_func, value_func))
+    print(map_custom({CustomMap({1: 2}, None, None): 1}, key_func, value_func))
