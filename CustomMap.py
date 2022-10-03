339d0ad60c2dc86c5714e43d75940fc406e0f722
Baston Sergey <bastonsv@gmail.com>
2022-10-03 21:03:16 +0300

    HW-23 Added generator refactor Custom_map

--- CustomMap.py
+++ CustomMap.py
@@ -6,15 +6,13 @@ class CustomMap:
 
     def custom_map(self):
         output_dict = {}
-        items = iter(self.in_dict.items())
-        for key_val_tuple in items:
+        for key_val_tuple in self.in_dict.items():
             output_dict.update({self.func_key(key_val_tuple[0]): self.func_value(key_val_tuple[1])})
         return output_dict
 
 
 def map_custom(in_dict: dict, func_key: object, func_value: object) -> dict:
     output_dict = {}
-    items = iter(in_dict.items())
-    for key_val_tuple in items:
+    for key_val_tuple in in_dict.items():
         output_dict.update({func_key(key_val_tuple[0]): func_value(key_val_tuple[1])})
     return output_dict
--- custom_map_example.py
+++ custom_map_example.py
@@ -1,17 +1,19 @@
+import datetime
+
 from CustomMap import CustomMap, map_custom
 
 
 def key_func(in_object: object) -> object:
-    return type(in_object)
+    return in_object * 2
 
 
 def value_func(in_object: object) -> object:
-    return type(in_object)
+    return in_object * 2
 
 
 if __name__ == "__main__":
     # Simple dataset and solution by class
-    print(CustomMap({True: 2, "two": 2}, key_func, value_func).custom_map())
+    print(CustomMap({False: 2, "two": 2, "three": True}, key_func, value_func).custom_map())
 
     # Class in dataset and solution by function
     print(map_custom({CustomMap({1: 2}, None, None): 1}, key_func, value_func))
