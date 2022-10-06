9825c3b52aad8ceba72465cd7c001d67ae0366ae
Baston Sergey <bastonsv@gmail.com>
2022-10-06 22:35:29 +0300

    HW-23 Custom_map

--- CustomMap.py
+++ CustomMap.py
@@ -12,10 +12,3 @@ class custom_map:
         key_val = next(self.iter_obj)
         return self.func_key(key_val[0]), self.func_val(key_val[1])
 
-
-def key_function(in_object: object) -> object:
-    return in_object * 2
-
-
-def value_function(in_object: object) -> object:
-    return in_object * 2
